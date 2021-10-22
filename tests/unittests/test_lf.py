7e13001b2fb0854b7bad9df68d25c385f70892eb
Sebastian Lee <sebastian@lf1.io>
2021-10-22 14:36:29 +0200

    Cleaned up tests

--- tests/unittests/test_lf.py
+++ tests/unittests/test_lf.py
@@ -106,47 +106,34 @@ class TestCompose:
     def test_output(self):
         assert self.transform_4(1) == 4
 
-
-def test_context():
-    assert plus_one.lf_stage is None
-    with plus_one.lf_set_stage('infer'):
-        assert plus_one.lf_stage is 'infer'
-        assert plus_one.lf_preprocess.lf_stage == 'infer'
-        assert plus_one.lf_forward.lf_stage == 'infer'
-        assert plus_one.lf_postprocess.lf_stage == 'infer'
+    def test_infer_apply(self):
+        assert self.transform_4.infer_apply(1) == 4
 
 
-def test_infer_apply():
-    assert plus_one.infer_apply(5) == 6
+class TestFunctionTransform:
+    @pytest.fixture(autouse=True, scope='class')
+    def init(self, request):
+        request.cls.transform_1 = plus_one
+        request.cls.transform_2 = get_info
 
+    def test_infer_apply(self):
+        assert self.transform_1.infer_apply(5) == 6
 
-def test_eval_apply():
-    out = list(plus_one.eval_apply([5, 6], flatten=False))
-    assert len(out) == 2
-    assert out[0] == 6
-    assert out[1] == 7
+    def test_eval_apply(self):
+        out = list(self.transform_1.eval_apply([5, 6], flatten=False))
+        assert len(out) == 2
+        assert out[0] == 6
+        assert out[1] == 7
 
-    out = list(get_info.eval_apply([{'info': 'hello'}, {'info': 'dog'}], flatten=False))
-    assert len(out) == 2
-    assert out[0] == 'hello'
-    assert out[1] == 'dog'
+        out = list(self.transform_2.eval_apply([{'info': 'hello'}, {'info': 'dog'}], flatten=False))
+        assert len(out) == 2
+        assert out[0] == 'hello'
+        assert out[1] == 'dog'
 
-
-def test_compose():
-    comp1 = lf.Compose([plus_one, plus_one], module=None, stack=None)
-    assert comp1(2) == 4
-    assert comp1.infer_apply(2) == 4
-
-    comp2 = lf.Compose([plus_one, plus_one, Batchify()], module=None, stack=None)
-    print(comp2.infer_apply(2))
-    print(list(comp2.eval_apply([2, 2])))
-    print(list(comp2.train_apply([2, 2])))
-
-
-# TODO Add back once I can test Compose with preprocess step
-# def test_loader_kwargs():
-#     plus_one = lf.trans(plus_one_)
-#     loader_kwargs = {'batch_size': 2}
-#     out = list(plus_one.eval_apply([5, 6, 7, 8], loader_kwargs=loader_kwargs, flatten=False))
-#     assert torch.all(out[0] == torch.tensor([6, 7]))
-#     assert torch.all(out[1] == torch.tensor([8, 9]))
+    def test_context(self):
+        assert self.transform_1.lf_stage is None
+        with self.transform_1.lf_set_stage('infer'):
+            assert self.transform_1.lf_stage is 'infer'
+            assert self.transform_1.lf_preprocess.lf_stage == 'infer'
+            assert self.transform_1.lf_forward.lf_stage == 'infer'
+            assert self.transform_1.lf_postprocess.lf_stage == 'infer'
