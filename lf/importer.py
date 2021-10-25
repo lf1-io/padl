c54b3d8d08537b94ffc3752b9e451d92a0e19930
Alexander Schlegel <wuhu@github.com>
2021-10-25 22:55:25 +0200

    improvements

---
+++ lf/importer.py
@@ -0,0 +1,29 @@
+import importlib
+import sys
+from types import ModuleType
+
+from lf import trans
+
+
+class PatchedModule:
+    def __init__(self, module):
+        self.module = module
+
+    def __getattr__(self, key):
+        x = self.module.__dict__[key]
+        if callable(x):
+            return trans(x)
+        if isinstance(x, ModuleType):
+            return PatchedModule(x)
+
+
+class Patcher:
+    def __getattr__(self, name):
+        try:
+            m = importlib.import_module(name)
+            return PatchedModule(m)
+        except:
+            pass
+
+
+sys.modules[__name__] = Patcher()
