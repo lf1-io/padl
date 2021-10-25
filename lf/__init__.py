c8d2f38d0b16cec27d5e5ff525f0f89768a98ab2
Alexander Schlegel <wuhu@github.com>
2021-10-25 17:43:13 +0200

    fix conflicts

--- lf/__init__.py
+++ lf/__init__.py
@@ -1,4 +1,3 @@
 from lf.wrap import trans
 
-from lf.transform import Identity, group
-from lf.util_transforms import Batchify, Unbatchify
+from lf.transform import Batchify, Unbatchify, Identity, group
