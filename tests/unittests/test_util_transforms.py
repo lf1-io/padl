76cbc52b283886d61d25b84dcce5724122b6c6e0
Sebastian Lee <sebastian@lf1.io>
2021-10-26 14:36:11 +0200

    Finished adding tests

--- tests/unittests/test_util_transforms.py
+++ tests/unittests/test_util_transforms.py
@@ -18,21 +18,36 @@ def times_two(x):
 class TestIfInStage:
     @pytest.fixture(autouse=True, scope='class')
     def init(self, request):
-        request.cls.transform_1 = plus_one >> IfTrain(times_two, plus_one)
-        request.cls.transform_2 = plus_one >> IfEval(times_two)
-        request.cls.transform_3 = plus_one >> IfInfer(times_two)
+        request.cls.transform_1 = (
+            plus_one
+            >> IfTrain(times_two, plus_one)
+            >> Batchify()
+            >> trans(lambda x: x * 3)
+        )
+        request.cls.transform_2 = (
+            plus_one
+            >> IfEval(times_two)
+            >> Batchify()
+            >> trans(lambda x: x * 3)
+        )
+        request.cls.transform_3 = (
+            plus_one
+            >> IfInfer(times_two)
+            >> Batchify()
+            >> trans(lambda x: x * 3)
+        )
 
     def test_infer_apply(self):
-        assert self.transform_1.infer_apply(1) == 3
-        assert self.transform_2.infer_apply(1) == 2
-        assert self.transform_3.infer_apply(1) == 4
+        assert self.transform_1.infer_apply(1) == 9
+        assert self.transform_2.infer_apply(1) == 6
+        assert self.transform_3.infer_apply(1) == 12
 
     def test_eval_apply(self):
-        assert list(self.transform_1.eval_apply([1, 2])) == [3, 4]
-        assert list(self.transform_2.eval_apply([1, 2])) == [4, 6]
-        assert list(self.transform_3.eval_apply([1, 2])) == [2, 3]
+        assert list(self.transform_1.eval_apply([1, 2])) == [9, 12]
+        assert list(self.transform_2.eval_apply([1, 2])) == [12, 18]
+        assert list(self.transform_3.eval_apply([1, 2])) == [6, 9]
 
     def test_train_apply(self):
-        assert list(self.transform_1.train_apply([1, 2])) == [4, 6]
-        assert list(self.transform_2.train_apply([1, 2])) == [2, 3]
-        assert list(self.transform_3.train_apply([1, 2])) == [2, 3]
+        assert list(self.transform_1.train_apply([1, 2])) == [12, 18]
+        assert list(self.transform_2.train_apply([1, 2])) == [6, 9]
+        assert list(self.transform_3.train_apply([1, 2])) == [6, 9]
