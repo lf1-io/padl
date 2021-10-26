5fa779f1f44303d14371622f7db7ef0646d7b8f2
Sebastian Lee <sebastian@lf1.io>
2021-10-26 13:09:36 +0200

    Adding more tests

--- tests/unittests/test_util_transforms.py
+++ tests/unittests/test_util_transforms.py
@@ -19,4 +19,15 @@ class TestIfInStage:
     @pytest.fixture(autouse=True, scope='class')
     def init(self, request):
         request.cls.transform_1 = plus_one >> IfTrain(times_two, plus_one)
-        request.cls.transform_2 = plus_one >> IfTrain(times_two)
+        request.cls.transform_2 = plus_one >> IfEval(times_two)
+        request.cls.transform_3 = plus_one >> IfInfer(times_two)
+
+    def test_infer_apply(self):
+        assert self.transform_1.infer_apply(1) == 3
+        assert self.transform_2.infer_apply(1) == 2
+        assert self.transform_3.infer_apply(1) == 4
+
+    def test_eval_apply(self):
+        assert list(self.transform_1.eval_apply([1, 2])) == [3, 4]
+        assert list(self.transform_2.eval_apply([1, 2])) == [4, 6]
+        assert list(self.transform_3.eval_apply([1, 2])) == [2, 3]
