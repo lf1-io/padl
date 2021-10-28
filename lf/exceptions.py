126212b56593808ef3bf5c5d3168337df531aa96
jasonkhadka <jasonkhadka33@gmail.com>
2021-10-28 17:14:11 +0200

    rebase

--- lf/exceptions.py
+++ lf/exceptions.py
@@ -8,12 +8,7 @@ class WrongDeviceError(Exception):
     :param child_transform: transform that is contained in mother transform
     """
     def __init__(self, mother_transform, child_transform):
-<<<<<<< HEAD
         self.mother_name = mother_transform.lf_name if mother_transform.lf_name is not None else mother_transform._lf_shortname()
         self.child_name = child_transform.lf_name if child_transform.lf_name is not None else child_transform._lf_shortname()
-=======
-        self.mother_name = mother_transform.lf_name if mother_transform.lf_name is not None else mother_transform.lf_shortname()
-        self.child_name = child_transform.lf_name if child_transform.lf_name is not None else child_transform.lf_shortname()
->>>>>>> c6e8ce8 (tests)
 
         super().__init__(f"{self.mother_name} is in '{mother_transform.lf_device}', while {self.child_name} is in '{child_transform.lf_device}'")
