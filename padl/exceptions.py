2af1942ce31edb106ad6db4aa28b792361261546
Alexander Schlegel <wuhu@github.com>
2021-10-30 21:12:11 +0200

    Cleanup + renaming.

--- padl/exceptions.py
+++ padl/exceptions.py
@@ -1,14 +1,13 @@
 """Exceptions"""
 
-
 class WrongDeviceError(Exception):
-    """Devices do not match for all transforms
+    """Devices do not match for all transforms.
 
-    :param mother_transform: main transform that contains child transform
-    :param child_transform: transform that is contained in mother transform
+    :param mother_transform: Main transform that contains child transform.
+    :param child_transform: Transform that is contained in mother transform.
     """
-    def __init__(self, mother_transform, child_transform):
-        self.mother_name = mother_transform.pd_name if mother_transform.pd_name is not None else mother_transform._pd_shortname()
-        self.child_name = child_transform.pd_name if child_transform.pd_name is not None else child_transform._pd_shortname()
 
-        super().__init__(f"{self.mother_name} is in '{mother_transform.pd_device}', while {self.child_name} is in '{child_transform.pd_device}'")
+    def __init__(self, mother_transform: "Transform", child_transform: "Transform"):
+        super().__init__(f"{mother_transform._pd_shortrepr()} is in '{mother_transform.pd_device}',"
+                         f' while {child_transform._pd_shortrepr()} is in '
+                         f"'{child_transform.pd_device}'")
