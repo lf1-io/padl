853f3302cb8272c704243c2cfaa81c3f743e75ba
Enrique Ramírez <enrique@lf1.io>
2021-11-15 15:25:51 +0100

    Test but saving not working

--- tests/unittests/test_util_transforms.py
+++ tests/unittests/test_util_transforms.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 from padl import transform, Batchify
 import padl.transforms as padl
 from padl.util_transforms import IfTrain, IfEval, IfInfer, Try
@@ -15,7 +16,26 @@ def times_two(x):
 
 
 times_three = transform(lambda x: x * 3)
-return_zero = transform(lambda x: 0)
+
+
+@transform
+class TensorMult(torch.nn.Module):
+    def __init__(self, factor):
+        super().__init__()
+        self.factor = factor
+
+    def forward(self, x):
+        return self.factor * x
+
+
+@transform
+class TensorDivide(torch.nn.Module):
+    def __init__(self, div):
+        super().__init__()
+        self.div = div
+
+    def forward(self, x):
+        return x / self.div
 
 
 class TestIfInStage:
@@ -90,26 +110,71 @@ class TestTry:
         request.cls.divide_by_zero = transform(lambda x: x / 0)
         request.cls.str_sum = transform(lambda x: x + 'a')
         request.cls.return_zero = transform(lambda x: 0)
-        request.cls.try_transform_1 = (plus_one
-                                       >> Batchify()
-                                       >> Try(self.divide_by_zero, self.str_sum,
-                                              (TypeError, ZeroDivisionError),
-                                              finally_transform=times_two)
-                                       >> plus_one
-                                       )
-        request.cls.try_transform_2 = (times_two
-                                       >> Try(self.divide_by_zero, self.return_zero,
-                                              ZeroDivisionError, finally_transform=plus_one)
-                                       >> Batchify()
-                                       >> times_three
-                                       )
+        request.cls.mult_two = TensorMult(2)
+        request.cls.mult_three = TensorMult(3)
+        request.cls.div_char = TensorDivide('a')
+        request.cls.try_transform_1 = (
+            Try(self.divide_by_zero,
+                times_two,
+                (TypeError, ZeroDivisionError),
+                plus_one,
+                plus_one)
+            >> Batchify()
+            >> plus_one
+        )
+        request.cls.try_transform_2 = (
+            plus_one
+            >> transform(lambda x: torch.LongTensor([x]))
+            >> Batchify()
+            >> Try(self.div_char, self.mult_two, TypeError, self.mult_two, self.mult_two)
+        )
+        request.cls.try_transform_3 = (
+            plus_one
+            >> transform(lambda x: torch.LongTensor([x]))
+            >> Batchify()
+            >> Try(self.mult_three, self.div_char, TypeError, self.div_char, self.mult_two)
+        )
 
     def test_infer_apply_1(self):
-        assert self.try_transform_1.infer_apply(4).item() == 13
+        assert self.try_transform_1.infer_apply(4).item() == 10
 
     def test_infer_apply_2(self):
-        assert self.try_transform_2.infer_apply(3).item() == 3
+        assert self.try_transform_2.infer_apply(3).item() == 16
+
+    def test_infer_apply_3(self):
+        assert self.try_transform_3.infer_apply(2).item() == 18
 
     def test_train_apply_1(self):
-        assert list(self.try_transform_1.train_apply([5, 8])) == [13, 19]
-        
+        assert list(self.try_transform_1.train_apply([5, 8])) == [12, 18]
+
+    def test_train_apply_2(self):
+        assert list(self.try_transform_2.train_apply([2, 3, 4])) == [12, 16, 20]
+
+    def test_train_apply_3(self):
+        assert list(self.try_transform_3.train_apply([1, 5, 2])) == [12, 36, 18]
+
+    def test_eval_apply_1(self):
+        assert list(self.try_transform_1.eval_apply([4, 9])) == [10, 20]
+
+    def test_eval_apply_2(self):
+        assert list(self.try_transform_2.eval_apply([3, 7])) == [16, 32]
+
+    def test_eval_apply_3(self):
+        assert list(self.try_transform_3.eval_apply([2, 6])) == [18, 42]
+
+    def test_save_and_load_1(self, tmp_path):
+        breakpoint()
+        self.try_transform_1.pd_save(tmp_path / 'test.padl')
+        t1 = padl.load(tmp_path / 'test.padl')
+        assert t1.infer_apply(4) == 10
+'''
+    def test_save_and_load_2(self, tmp_path):
+        self.try_transform_2.pd_save(tmp_path / 'test.padl')
+        t2 = padl.load(tmp_path / 'test.padl')
+        assert t2.infer_apply(3) == 16
+
+    def test_save_and_load_3(self, tmp_path):
+        self.try_transform_3.pd_save(tmp_path / 'test.padl')
+        t3 = padl.load(tmp_path / 'test.padl')
+        assert t3.infer_apply(2) == 18
+'''
