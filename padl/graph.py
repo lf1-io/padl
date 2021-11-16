2739fa23d375831ceacd39e3d5c2723a0ad6b89e
jasonkhadka <jasonkhadka33@gmail.com>
2021-11-16 15:15:58 +0100

    wip

---
+++ padl/graph.py
@@ -0,0 +1,131 @@
+
+
+class Graph:
+    _node_id = 0
+
+    def __init__(self, transform=None):
+        if transform is not None:
+            self.start_node = Node(transform, graph=self, id=self._node_id)
+        self.input_node = InputNode()
+        self.output_node = OutputNode()
+        self.nodes = [self.input_node, self.input_node, self.output_node]
+
+    def add_node(self, node):
+        if node in self.nodes:
+            return
+        self.nodes.append(node)
+        node.graph = self
+        node.update_id(self.get_next_node_id())
+
+    @classmethod
+    def get_next_node_id(cls):
+        cls._node_id += 1
+        return cls._node_id
+
+
+class Node:
+
+    def __init__(self, transform, graph=None, id=None, input=None, output=None):
+        if graph is not None:
+            self.graph = graph
+        else:
+            self._graph = graph
+        self.id = id if id is not None else 0
+        self.input_node = self._init_input(input)
+        self.output_node = self._init_output(output)
+        self._input_args = {node: None for node in self.input_node}
+        self._updated_args = []
+        self.transform = transform
+
+    @property
+    def graph(self):
+        return self._graph
+
+    @graph.setter
+    def graph(self, new_graph):
+        self._graph = new_graph
+        new_graph.add_node(self)
+
+    def _drop_inbuilt_output_node(self):
+        out_nodes = [out_node for out_node in self.output_node if not isinstance(out_node, OutputNode)]
+        self.output_node = out_nodes
+
+    def _drop_inbuilt_input_node(self):
+        in_nodes = [in_node for in_node in self.input_node if not isinstance(in_node, InputNode)]
+        self.input_node = in_nodes
+
+    def update_output_node(self, node):
+        self._drop_inbuilt_output_node()
+        if self.graph != node.graph:
+            self.graph.add_node(node)
+        self.output_node = [node]
+        node.insert_input_node(self)
+
+    def insert_output_node(self, node):
+        self._drop_inbuilt_output_node()
+        if self.graph != node.graph:
+            self.graph.add_node(node)
+        self.output_node.append(node)
+        node.insert_input_node(self)
+
+    def update_input_node(self, node):
+        self._drop_inbuilt_input_node()
+        if self.graph != node.graph:
+            self.graph.add_node(node)
+        self.input_node = [node]
+        node.insert_output_node(self)
+
+    def insert_input_node(self, node):
+        self._drop_inbuilt_input_node()
+        if self.graph != node.graph:
+            self.graph.add_node(node)
+        self.input_node.append(node)
+        node.insert_output_node(self)
+
+    def _init_input(self, input):
+        if input is None:
+            return [InputNode()]
+        return [input]
+
+    def _init_output(self, output):
+        if output is None:
+            return [OutputNode()]
+        return [output]
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def _check_input_complete(self):
+        return all([True if in_node in self._updated_args else False for in_node in self.input_node])
+
+    def _register_input_args(self, args, node):
+        self._input_args[node] = args
+        self._updated_args.append(node)
+
+    def _pd_call_transform(self):
+        args = list(self._input_args.values())
+        if len(args) == 1:
+            return self.transform._pd_call_transform(args[0])
+        return self.transform._pd_call_transform(tuple(args))
+
+    def __call__(self, args, node):
+        self._register_input_args(args, node)
+        if self._check_input_complete():
+            return self._pd_call_transform()
+        return
+
+
+
+class InputNode:
+
+    def __init__(self, graph=None, id=None):
+        self.graph = graph
+        self.id = id if id is not None else 0
+
+
+class OutputNode:
+
+    def __init__(self, graph=None, id=None):
+        self.graph = graph
+        self.id = id if id is not None else 0
+
