2bc23325f9357d0ae3c0fef51d7f536712b55ab3
jasonkhadka <jasonkhadka33@gmail.com>
2021-11-18 10:11:12 +0100

    wip

--- padl/graph.py
+++ padl/graph.py
@@ -1,26 +1,19 @@
+import padl
 
 
 class Graph:
     _node_id = 0
 
-    def __init__(self, transform=None):
+    def __init__(self):
         self.nodes = []
-        self.start_node = None
-        if transform is not None:
-            self.start_node = Node(transform, graph=self, id=self._node_id)
         self.input_node = InputNode(graph=self)
         self.output_node = OutputNode(graph=self)
-        self.nodes.extend([self.input_node, self.output_node])
-
-        if self.start_node:
-            self.start_node.input_node = [self.input_node]
-            self.start_node.output_node = [self.output_node]
 
     def add_node(self, node):
         if node in self.nodes:
             return
         self.nodes.append(node)
-        node.graph = self
+        node.update_graph(self)
         node.update_id(self.get_next_node_id())
 
     @classmethod
@@ -29,20 +22,19 @@ class Graph:
         return cls._node_id
 
 
-class Node:
+class BaseNode:
 
-    def __init__(self, transform, graph=None, id=None, input=None, output=None):
+    def __init__(self, transform, graph=None, id=None, in_node=None, out_node=None):
         self._graph = None
         if graph is not None:
             self.graph = graph
+            id = id if id is not None else self.graph.get_next_node_id()
 
-        self.id = id if id is not None else 0
-        if self.graph is None:
-            self.input_node = self._init_input(input)
-            self.output_node = self._init_output(output)
-        self._input_args = {node: None for node in self.input_node}
-        self._updated_args = []
         self.transform = transform
+        self.id = id
+
+        self.in_node = in_node
+        self.out_node = out_node
 
     @property
     def graph(self):
@@ -50,10 +42,19 @@ class Node:
 
     @graph.setter
     def graph(self, new_graph):
-        self._graph = new_graph
         if new_graph is None:
             return
+        if self.graph == new_graph:
+            return
+        self._graph = new_graph
         new_graph.add_node(self)
+
+    def update_graph(self, new_graph):
+        if self.graph == new_graph:
+            return
+        self.graph = new_graph
+        if self.graph is None:
+            return
 
     def update_id(self, new_id):
         self.id = new_id
@@ -66,20 +67,32 @@ class Node:
         for node in self.input_node:
             yield node
 
+
+class Node(BaseNode):
+
+    def __init__(self, transform, graph=None, id=None, in_node=None, out_node=None):
+        super().__init__(transform, graph, id, in_node, out_node)
+        if self.graph is None:
+            self.in_node = self._init_input(in_node)
+            self.out_node = self._init_output(out_node)
+        self._input_args = {node: None for node in self.in_node}
+        self._updated_args = []
+        self.transform = transform
+
     def _drop_inbuilt_output_node(self):
-        out_nodes = [out_node for out_node in self.output_node if not isinstance(out_node, OutputNode)]
-        self.output_node = out_nodes
+        out_nodes = [out_node for out_node in self.out_node if not isinstance(out_node, OutputNode)]
+        self.out_node = out_nodes
 
     def _drop_inbuilt_input_node(self):
-        in_nodes = [in_node for in_node in self.input_node if not isinstance(in_node, InputNode)]
-        self.input_node = in_nodes
+        in_nodes = [in_node for in_node in self.in_node if not isinstance(in_node, InputNode)]
+        self.in_node = in_nodes
 
     def update_output_node(self, node):
         self._drop_inbuilt_output_node()
         if self.graph != node.graph:
             self.graph.add_node(node)
-        self.output_node = [node]
-        if self in node.input_node:
+        self.out_node = [node]
+        if self in node.in_node:
             return
         node.insert_input_node(self)
 
@@ -87,8 +100,8 @@ class Node:
         self._drop_inbuilt_output_node()
         if self.graph != node.graph:
             self.graph.add_node(node)
-        self.output_node.append(node)
-        if self in node.input_node:
+        self.out_node.append(node)
+        if self in node.in_node:
             return
         node.insert_input_node(self)
 
@@ -96,8 +109,8 @@ class Node:
         self._drop_inbuilt_input_node()
         if self.graph != node.graph:
             self.graph.add_node(node)
-        self.input_node = [node]
-        if self in node.output_node:
+        self.in_node = [node]
+        if self in node.out_node:
             return
         node.insert_output_node(self)
 
@@ -105,8 +118,8 @@ class Node:
         self._drop_inbuilt_input_node()
         if self.graph != node.graph:
             self.graph.add_node(node)
-        self.input_node.append(node)
-        if self in node.output_node:
+        self.in_node.append(node)
+        if self in node.out_node:
             return
         node.insert_output_node(self)
 
@@ -124,7 +137,7 @@ class Node:
         return hash(repr(self))
 
     def _check_input_complete(self):
-        return all([True if in_node in self._updated_args else False for in_node in self.input_node])
+        return all([True if in_node in self._updated_args else False for in_node in self.in_node])
 
     def _register_input_args(self, args, node):
         self._input_args[node] = args
@@ -144,16 +157,13 @@ class Node:
 
 
 
-class InputNode(Node):
+class InputNode(BaseNode):
 
     def __init__(self, graph=None, id=None):
-        self.graph = graph
-        self.id = id if id is not None else 0
+        super().__init__(padl.Identity(), graph=graph, id=id)
 
 
-class OutputNode(Node):
+class OutputNode(BaseNode):
 
     def __init__(self, graph=None, id=None):
-        self.graph = graph
-        self.id = id if id is not None else 0
-
+        super().__init__(padl.Identity(), graph=graph, id=id)
