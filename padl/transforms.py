ad2a029497c1f4cbc1074dc7a1c042dbed367da7
Sebastian Lee <sebastian@lf1.io>
2022-01-03 09:38:04 +0100

    Removed if statement in _ItemGetter since it creates overhead for every worker in the DataLoader even though it is never used and is used in a hidden class.

--- padl/transforms.py
+++ padl/transforms.py
@@ -2180,22 +2180,13 @@ class _ItemGetter:
 
     :param samples: An object implementing __getitem__ and __len__.
     :param transform: Preprocessing transform.
-    :param exception: Exception to catch for (fall back to *default*).
-    :param default: The default value to fall back to in case of exception.
     """
 
-    def __init__(self, samples, transform, exception=None, default=None):
+    def __init__(self, samples, transform):
         self.samples = samples
         self.transform = transform
-        self.exception = exception
-        self.default = default
 
     def __getitem__(self, item):
-        if self.exception:
-            try:
-                return self.transform(self.samples[item])
-            except self.exception:
-                return self.default
         return self.transform(self.samples[item])
 
     def __len__(self):
