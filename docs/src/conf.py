d775f8cbad83b05c93f40be2e5eb5c16a1386079
Sebastian Lee <sebastian@lf1.io>
2022-01-13 16:40:59 +0100

    Update Makefile, and usage/combin_transforms.md

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -81,7 +81,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['img']
+html_static_path = ['static']
 html_css_files = ["custom.css"]
 html_show_sourcelink = False
 
