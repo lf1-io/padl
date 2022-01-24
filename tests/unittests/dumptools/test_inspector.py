d05ac78dbf85527538e63e92d2762734fc78b282
Alexander Schlegel <wuhu@github.com>
2022-01-24 16:15:29 +0100

    Add tests for get_surrounding_block.

--- tests/unittests/dumptools/test_inspector.py
+++ tests/unittests/dumptools/test_inspector.py
@@ -32,7 +32,7 @@ class TestCallerInfo:
             double_nested_builder()
 
 
-testcode = '''
+testcode_0 = '''
 a = 1
 b = 2
 
@@ -52,33 +52,64 @@ def c():
 '''
 
 
+testcode_1 = '''
+def c(one=1
+      two=2):
+    """
+    this is a comment
+    """
+    o = 7
+    p = (a,
+        b,
+        c)
+
+    f = a + \
+        b
+
+    k = f(1,
+          2, 3,
+          4)
+    return f
+'''
+
+
 class TestGetStatement:
     def test_simple(self):
-        statement, _ = inspector.get_statement(testcode, 2)
+        statement, _ = inspector.get_statement(testcode_0, 2)
         assert statement == 'a = 1'
-        statement, _ = inspector.get_statement(testcode, 3)
+        statement, _ = inspector.get_statement(testcode_0, 3)
         assert statement == 'b = 2'
 
     def test_function(self):
-        statement, _ = inspector.get_statement(testcode, 5)
-        assert statement == testcode.split(' = 2')[1].strip()
+        statement, _ = inspector.get_statement(testcode_0, 5)
+        assert statement == testcode_0.split(' = 2')[1].strip()
 
     def test_multiline_1(self):
-        statement, _ = inspector.get_statement(testcode, 7)
+        statement, _ = inspector.get_statement(testcode_0, 7)
         assert statement == 'p = (a,\n    b,\n    c)'
 
     def test_multiline_2(self):
-        statement, _ = inspector.get_statement(testcode, 8)
+        statement, _ = inspector.get_statement(testcode_0, 8)
         assert statement == 'p = (a,\n    b,\n    c)'
 
     def test_multiline_3(self):
-        statement, _ = inspector.get_statement(testcode, 9)
+        statement, _ = inspector.get_statement(testcode_0, 9)
         assert statement == 'p = (a,\n    b,\n    c)'
 
     def test_multiline_4(self):
-        statement, _ = inspector.get_statement(testcode, 11)
+        statement, _ = inspector.get_statement(testcode_0, 11)
         assert statement == 'f = a +         b'
 
     def test_multiline_5(self):
-        statement, _ = inspector.get_statement(testcode, 14)
+        statement, _ = inspector.get_statement(testcode_0, 14)
         assert statement == 'k = f(1,\n      2, 3,\n      4)'
+
+
+class TestGetSurroundingBlock:
+    def test_hanging_arguments_1(self):
+        block, l, r = inspector.get_surrounding_block(testcode_1, 7)
+        assert block.strip.startswith('"""')
+
+    def test_hanging_arguments_2(self):
+        block, l, r = inspector.get_surrounding_block(testcode_1, 10)
+        assert block.strip.startswith('"""')
