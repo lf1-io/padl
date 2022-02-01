658c5ab2b249ca804edec69e57a2dec168a4d494
Alexander Schlegel <wuhu@github.com>
2022-02-01 16:46:34 +0100

    Use correct scope.

--- padl/dumptools/serialize.py
+++ padl/dumptools/serialize.py
@@ -40,9 +40,9 @@ class Serializer:
         if module is None:
             module = inspector.caller_module()
         self.scope = symfinder.Scope.toplevel(module)
-        self.load_codegraph = (
-            var2mod.CodeGraph.build(ScopedName(load_function.__name__, self.scope))
-        )
+        self.load_codegraph = \
+            var2mod.CodeGraph.build(ScopedName(load_function.__name__,
+                                               symfinder.Scope.toplevel(load_function.__module__)))
         self.load_name = load_function.__name__
         super().__init__()
 
